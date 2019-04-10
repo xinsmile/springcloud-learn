6596e09f9004bb056d2b1149af573e243c3c5fd4
xinguiyuan <614376875@qq.com>
2019-04-10 15:22:11 +0800

    【eureka】

---
+++ spring-cloud-core/src/main/java/com/xin/cloud/core/SpringCloudCoreApplication.java
@@ -0,0 +1,13 @@
+package com.xin.cloud.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudCoreApplication.class, args);
+	}
+
+}
